d81dae6d04018628e5166a1dce87f0cf030847d3
-XO-OX- <greshamd27@gmail.com>
2026-04-12 14:09:08 +0700

    Test release + daily update

--- assets/js/site-style.js
+++ assets/js/site-style.js
@@ -4,31 +4,30 @@
 	const PATHS = [
 		{
 			home: 'index.html',
-			about: 'pages/about.html',
-			project: 'pages/project.html',
+			about: 'pages/about-me.html',
+			projects: 'pages/projects.html',
+			devdocs: 'pages/the-dev-docs.html',
+			contact: 'pages/contact-me.html',
 			resume: 'assets/documents/matt_gresham_resume.pdf',
-			contact: 'pages/contact.html',
-			blog: 'pages/blog.html',
 		},
 		{
 			home: '../index.html',
-			about: '../pages/about.html',
-			project: '../pages/project.html',
+			about: 'about-me.html',
+			projects: 'projects.html',
+			devdocs: 'the-dev-docs.html',
+			contact: 'contact-me.html',
 			resume: '../assets/documents/matt_gresham_resume.pdf',
-			contact: '../pages/contact.html',
-			blog: '../pages/blog.html',
 		},
 		{
 			home: '../../index.html',
-			about: '../../pages/about.html',
-			project: '../../pages/project.html',
+			about: '../about-me.html',
+			projects: '../projects.html',
+			devdocs: '../the-dev-docs.html',
+			contact: '../contact-me.html',
 			resume: '../../assets/documents/matt_gresham_resume.pdf',
-			contact: '../../pages/contact.html',
-			blog: '../../pages/blog.html',
 		},
 	];
 	const p = PATHS[Math.min(depth, 2)];
-	const GH = 'https://github.com/PyMite6941';
 
 	function inject(id, html) {
 		const el = document.getElementById(id);
@@ -39,40 +38,12 @@
 		'site-nav',
 		`<div class="nav">
 			<ul class="nav-list">
-				<li class="nav-item"
-					><a
-						class="nav-link"
-						href="../index.html"
-						>Home</a
-					></li
-				>
-				<li class="nav-item"
-					><a
-						class="nav-link"
-						href="about-me.html"
-						>About Me</a
-					></li
-				>
-				<li class="nav-item"
-					><a
-						class="nav-link"
-						href="projects.html"
-						>Projects</a
-					></li
-				>
-				<li class="nav-item"
-					><a
-						class="nav-link"
-						href="the-dev-docs.html"
-						>The Dev Docs</a
-					></li
-				>
+				<li class="nav-item"><a class="nav-link" href="${p.home}">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="${p.about}">About Me</a></li>
+				<li class="nav-item"><a class="nav-link" href="${p.projects}">Projects</a></li>
+				<li class="nav-item"><a class="nav-link" href="${p.devdocs}">The Dev Docs</a></li>
 			</ul>
-			<a
-				class="nav-btn"
-				href="./contact-me.html"
-				>Contact me</a
-			>
+			<a class="nav-btn" href="${p.contact}">Contact me</a>
 		</div>`,
 	);
 
@@ -80,29 +51,13 @@
 		'site-footer',
 		`<footer class="foot">
 			<p>
-				<a
-					class="text-link"
-					href="https://github.com/PyMite6941"
-					>My GitHub</a
-				>
-				<br />
-				<br />
-				<a
-					class="text-link"
-					href="the-dev-docs.html"
-					>The Dev Docs</a
-				>
-				<br />
-				<br />
-				<a
-					class="text-link"
-					href="../assets/documents/matt_gresham_resume.pdf"
-					>View my resume</a
-				>
+				<a class="text-link" href="https://github.com/PyMite6941">My GitHub</a>
+				<br /><br />
+				<a class="text-link" href="${p.devdocs}">The Dev Docs</a>
+				<br /><br />
+				<a class="text-link" href="${p.resume}">View my resume</a>
 			</p>
-			<p style="text-align: center; font-size: 14px">
-				&copy; 2026 Matt Gresham. All rights reserved.
-			</p>
+			<p style="text-align: center; font-size: 14px">&copy; 2026 Matt Gresham. All rights reserved.</p>
 		</footer>`,
 	);
 })();
